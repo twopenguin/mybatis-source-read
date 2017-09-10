32e561d1b7a8812e67ebe8734f5ddccef9eba641
twopenguin <1289410799@qq.com>
2017-09-10 22:57:35 +0800

    mybatis插件学习，初步提交！

--- mybatis/src/main/java/com/zcd/mybatis/plugin/SqlPrintInterceptor.java
+++ mybatis/src/main/java/com/zcd/mybatis/plugin/SqlPrintInterceptor.java
@@ -1,5 +1,6 @@
 package com.zcd.mybatis.plugin;
 
+import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
@@ -16,15 +17,19 @@ import java.util.Properties;
 /**
  * Created by admin on 2017/9/7.
  */
-@Intercepts({@Signature(type= StatementHandler.class, method = "query",args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
+@Intercepts({@Signature(type= Executor.class, method = "query",args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
 public class SqlPrintInterceptor implements Interceptor {
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        StatementHandler handler = (StatementHandler)invocation.getTarget();
-        BoundSql sql = handler.getBoundSql();
-        invocation.getTarget();
-        return null;
+        MappedStatement statement =  (MappedStatement)invocation.getArgs()[0];
+        Object parameter = null;
+        if(invocation.getArgs().length > 1){
+            parameter = invocation.getArgs()[1];
+        }
+        BoundSql sql = statement.getBoundSql(parameter);
+        System.out.println(sql);
+        return invocation.proceed();
     }
 
     @Override
